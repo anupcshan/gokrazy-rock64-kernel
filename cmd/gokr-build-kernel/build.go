fae11c0881efff3f7013ce4dd2bef18ad1d0cd8a
Anup Chenthamarakshan <anupcshan@gmail.com>
2021-11-10 21:12:01 -0800

    rock64 kernel and u-boot
    - Based on https://github.com/anupcshan/gokrazy-odroidxu4-kernel/
    - Build functioning u-boot-rockchip.bin
    - Generate basic kernel and clean up XU4 files
    - Rename vmlinux -> vmlinuz to make Gokrazy plumbing easier
    - Enable squashfs root config
    - Enable STMMAC - network works, but eth has the same MAC address for all devices :\
    - Enable other Rockchip config entities
    - Fix setexpr string termination

---
+++ cmd/gokr-build-kernel/build.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+
+	_ "embed"
+)
+
+//go:embed config.txt
+var configContents []byte
+
+// see https://www.kernel.org/releases.json
+var latest = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.7.tar.xz"
+
+const firmwareSource = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/%s?id=%s"
+const firmwareRevision = "eb8ea1b46893c42edbd516f971a93b4d097730ab"
+const firmwareLocation = "/tmp/firmware"
+
+var firmwareFiles = []string{}
+
+func downloadKernel() error {
+	out, err := os.Create(filepath.Base(latest))
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	resp, err := http.Get(latest)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if got, want := resp.StatusCode, http.StatusOK; got != want {
+		return fmt.Errorf("unexpected HTTP status code for %s: got %d, want %d", latest, got, want)
+	}
+	if _, err := io.Copy(out, resp.Body); err != nil {
+		return err
+	}
+	return out.Close()
+}
+
+func downloadFirmware() error {
+	for _, firmwareFile := range firmwareFiles {
+		dir := filepath.Dir(firmwareFile)
+		if dir != "" {
+			if err := os.MkdirAll(filepath.Join(firmwareLocation, dir), 0755); err != nil {
+				return err
+			}
+		}
+		out, err := os.Create(filepath.Join(firmwareLocation, firmwareFile))
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+		resp, err := http.Get(fmt.Sprintf(firmwareSource, firmwareFile, firmwareRevision))
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		if got, want := resp.StatusCode, http.StatusOK; got != want {
+			return fmt.Errorf("unexpected HTTP status code for %s: got %d, want %d", firmwareFile, got, want)
+		}
+		if _, err := io.Copy(out, resp.Body); err != nil {
+			return err
+		}
+		if err := out.Close(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func applyPatches(srcdir string) error {
+	patches, err := filepath.Glob("*.patch")
+	if err != nil {
+		return err
+	}
+	for _, patch := range patches {
+		log.Printf("applying patch %q", patch)
+		f, err := os.Open(patch)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		cmd := exec.Command("patch", "-p1")
+		cmd.Dir = srcdir
+		cmd.Stdin = f
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+		f.Close()
+	}
+
+	return nil
+}
+
+func compile() error {
+	f, err := os.OpenFile(".config", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if _, err := f.Write(configContents); err != nil {
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	olddefconfig := exec.Command("make", "ARCH=arm64", "oldconfig")
+	olddefconfig.Stdout = os.Stdout
+	olddefconfig.Stderr = os.Stderr
+	if err := olddefconfig.Run(); err != nil {
+		return fmt.Errorf("make olddefconfig: %v", err)
+	}
+
+	make := exec.Command("make", "Image", "dtbs", "-j"+strconv.Itoa(runtime.NumCPU()))
+	make.Env = append(os.Environ(),
+		"ARCH=arm64",
+		"CROSS_COMPILE=aarch64-linux-gnu-",
+		"KBUILD_BUILD_USER=gokrazy",
+		"KBUILD_BUILD_HOST=docker",
+		"KBUILD_BUILD_TIMESTAMP=Wed Mar  1 20:57:29 UTC 2017",
+	)
+	make.Stdout = os.Stdout
+	make.Stderr = os.Stderr
+	if err := make.Run(); err != nil {
+		return fmt.Errorf("make: %v", err)
+	}
+
+	return nil
+}
+
+func copyFile(dest, src string) error {
+	out, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	if _, err := io.Copy(out, in); err != nil {
+		return err
+	}
+
+	st, err := in.Stat()
+	if err != nil {
+		return err
+	}
+	if err := out.Chmod(st.Mode()); err != nil {
+		return err
+	}
+	return out.Close()
+}
+
+func main() {
+	log.Printf("downloading firmware")
+	if err := os.MkdirAll(firmwareLocation, 0755); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := downloadFirmware(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("downloading kernel source: %s", latest)
+	if err := downloadKernel(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("unpacking kernel source")
+	untar := exec.Command("tar", "xf", filepath.Base(latest))
+	untar.Stdout = os.Stdout
+	untar.Stderr = os.Stderr
+	if err := untar.Run(); err != nil {
+		log.Fatalf("untar: %v", err)
+	}
+
+	srcdir := strings.TrimSuffix(filepath.Base(latest), ".tar.xz")
+
+	log.Printf("applying patches")
+	if err := applyPatches(srcdir); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := os.Chdir(srcdir); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("compiling kernel")
+	if err := compile(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := copyFile("/tmp/buildresult/vmlinuz", "arch/arm64/boot/Image"); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := copyFile("/tmp/buildresult/rk3328-rock64.dtb", "arch/arm64/boot/dts/rockchip/rk3328-rock64.dtb"); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ cmd/gokr-build-uboot/build.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strconv"
+)
+
+const ubootRev = "2ee7a8ec6f1711abe9619fd8765edc16742be9de"
+const ubootTS = 1704990307
+const trustedRepoRev = "1c76dd2d6f6733638c77ad20e6a620b607696dce"
+
+const (
+	uBootRepo           = "https://github.com/u-boot/u-boot"
+	trustedFirmwareRepo = "https://github.com/ARM-software/arm-trusted-firmware"
+)
+
+func applyPatches(srcdir string) error {
+	patches, err := filepath.Glob("*.patch")
+	if err != nil {
+		return err
+	}
+	for _, patch := range patches {
+		log.Printf("applying patch %q", patch)
+		f, err := os.Open(patch)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		cmd := exec.Command("patch", "-p1")
+		cmd.Dir = srcdir
+		cmd.Stdin = f
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+		f.Close()
+	}
+
+	return nil
+}
+
+func compile(trustedFirmwareDir string) error {
+	defconfig := exec.Command("make", "ARCH=arm64", "rock64-rk3328_defconfig")
+	defconfig.Stdout = os.Stdout
+	defconfig.Stderr = os.Stderr
+	if err := defconfig.Run(); err != nil {
+		return fmt.Errorf("make defconfig: %v", err)
+	}
+
+	f, err := os.OpenFile(".config", os.O_RDWR|os.O_APPEND, 0755)
+	if err != nil {
+		return err
+	}
+	if _, err := f.Write([]byte("CONFIG_CMD_SETEXPR=y\nCONFIG_CMD_SETEXPR_FMT=y\n")); err != nil {
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	make := exec.Command("make", "-j"+strconv.Itoa(runtime.NumCPU()))
+	make.Env = append(os.Environ(),
+		"ARCH=arm64",
+		"CROSS_COMPILE=aarch64-linux-gnu-",
+		"SOURCE_DATE_EPOCH="+strconv.Itoa(ubootTS),
+		fmt.Sprintf("BL31=%s/build/rk3328/release/bl31/bl31.elf", trustedFirmwareDir),
+	)
+	make.Stdout = os.Stdout
+	make.Stderr = os.Stderr
+	if err := make.Run(); err != nil {
+		return fmt.Errorf("make: %v", err)
+	}
+
+	return nil
+}
+
+func generateBootScr(bootCmdPath string) error {
+	mkimage := exec.Command("./tools/mkimage", "-A", "arm", "-T", "script", "-C", "none", "-d", bootCmdPath, "boot.scr")
+	mkimage.Env = append(os.Environ(),
+		"ARCH=arm64",
+		"CROSS_COMPILE=aarch64-linux-gnu-",
+		"SOURCE_DATE_EPOCH=1600000000",
+	)
+	mkimage.Stdout = os.Stdout
+	mkimage.Stderr = os.Stderr
+	if err := mkimage.Run(); err != nil {
+		return fmt.Errorf("mkimage: %v", err)
+	}
+
+	return nil
+}
+
+func copyFile(dest, src string) error {
+	out, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	if _, err := io.Copy(out, in); err != nil {
+		return err
+	}
+
+	st, err := in.Stat()
+	if err != nil {
+		return err
+	}
+	if err := out.Chmod(st.Mode()); err != nil {
+		return err
+	}
+	return out.Close()
+}
+
+func main() {
+	ubootDir, err := os.MkdirTemp("", "u-boot")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	trustedFirmwareDir, err := os.MkdirTemp("", "arm-trusted-firmware")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, cmd := range [][]string{
+		{"git", "init"},
+		{"git", "remote", "add", "origin", trustedFirmwareRepo},
+		{"git", "fetch", "--depth=1", "origin", trustedRepoRev},
+		{"git", "checkout", "FETCH_HEAD"},
+		{"make", "SOURCE_DATE_EPOCH=1600000000", "CROSS_COMPILE=aarch64-linux-gnu-", "PLAT=rk3328"},
+	} {
+		log.Printf("Running %s", cmd)
+		cmdObj := exec.Command(cmd[0], cmd[1:]...)
+		cmdObj.Stdout = os.Stdout
+		cmdObj.Stderr = os.Stderr
+		cmdObj.Dir = trustedFirmwareDir
+		if err := cmdObj.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	var bootCmdPath string
+	if p, err := filepath.Abs("boot.cmd"); err != nil {
+		log.Fatal(err)
+	} else {
+		bootCmdPath = p
+	}
+
+	if err := os.Chdir(ubootDir); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, cmd := range [][]string{
+		{"git", "init"},
+		{"git", "remote", "add", "origin", uBootRepo},
+		{"git", "fetch", "--depth=1", "origin", ubootRev},
+		{"git", "checkout", "FETCH_HEAD"},
+	} {
+		log.Printf("Running %s", cmd)
+		cmdObj := exec.Command(cmd[0], cmd[1:]...)
+		cmdObj.Stdout = os.Stdout
+		cmdObj.Stderr = os.Stderr
+		cmdObj.Dir = ubootDir
+		if err := cmdObj.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Printf("applying patches")
+	if err := applyPatches(ubootDir); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("compiling uboot")
+	if err := compile(trustedFirmwareDir); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("generating boot.scr")
+	if err := generateBootScr(bootCmdPath); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, copyCfg := range []struct {
+		dest, src string
+	}{
+		{"boot.scr", "boot.scr"},
+		{"u-boot-rockchip.bin", "u-boot-rockchip.bin"},
+	} {
+		if err := copyFile(filepath.Join("/tmp/buildresult", copyCfg.dest), copyCfg.src); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
