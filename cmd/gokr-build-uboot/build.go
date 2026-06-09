6a8ba6c5e46e2a2f17c403f31517ad8349c5ee28
gokrazy-bot-2 <95002936+gokrazy-bot-2@users.noreply.github.com>
2026-06-08 18:33:32 -0700

    auto-update u-boot to https://github.com/u-boot/u-boot/commit/1296a428c67cf103eca482d4a63349661c1b799f

--- cmd/gokr-build-uboot/build.go
+++ cmd/gokr-build-uboot/build.go
@@ -11,8 +11,8 @@ import (
 	"strconv"
 )
 
-const ubootRev = "56cac250b0839ddbad1311d3ca4231f532b5aadf"
-const ubootTS = 1766510257
+const ubootRev = "1296a428c67cf103eca482d4a63349661c1b799f"
+const ubootTS = 1780947784
 const trustedRepoRev = "10eb851f92acc67f7cdb955770e3bdced3026677"
 
 const (
